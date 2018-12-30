29206ff6e4cc88627cb47e108b3fee7da383cda2
Gokul <importfunfromcode@gmail.com>
2018-12-29 21:15:21 -0800

    hsv colour mask

--- colour-picker.py
+++ colour-picker.py
@@ -7,6 +7,11 @@ while True:
     ret, frame = cap.read()
     cv2.imshow("Frame image", frame)
 
+    hsvImage=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    Lower_hsv = np.array([110, 50, 50])
+    Upper_hsv = np.array([130,255,255])
+    mask=cv2.inRange(hsvImage,Lower_hsv,Upper_hsv)
+    cv2.imshow("colour mask",mask)
     key=cv2.waitKey(30) & 0xFF
     if key == 27: # exit on ESC
         break
