3dd492ccf1e8631cc4d6e6d89e809d8c73b797b0
Gokul <importfunfromcode@gmail.com>
2019-01-07 20:20:28 +0530

    capture and calibrate

--- colour-caliber.py
+++ colour-caliber.py
@@ -6,14 +6,15 @@ import sys
 app = QtWidgets.QApplication([])
 window = uic.loadUi("colour-caliber.ui")
 
+pix_x = pix_y = 0
 calib_gain = [1,1,1]
+
 
 def colour_change():
   global calib_gain
   calib_gain[0] = window.slider_blue.value()/100
   calib_gain[1] = window.slider_green.value()/100
   calib_gain[2] = window.slider_red.value()/100
-  print(calib_gain)
   window.label_blue.setText(str(calib_gain[0]))
   window.label_green.setText(str(calib_gain[1]))
   window.label_red.setText(str(calib_gain[2]))
@@ -32,9 +33,9 @@ window.slider_red.valueChanged.connect(colour_change)
 cap = cv2.VideoCapture(0) 
 
 def picker(event,x,y,flags,param):
-  print('> x =',x,'y =',y, 'colour =',param[y, x])
+  global pix_x,pix_y
   if event == cv2.EVENT_LBUTTONDOWN:
-    pix = param[y, x]
+    pix_y, pix_x =  y, x
 
 
 def color_calibration(image,ch_gain):
@@ -48,10 +49,12 @@ def color_calibration(image,ch_gain):
 
 colour_change()
 window.show()
-
+capture_disable = False
 while True:
-  ret, frame = cap.read()
-  frame = cv2.flip(frame, 1)
+  if capture_disable == False:
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+#  frame = cv2.imread('colour-chart.jpg')
   cv2.imshow("Frame image", frame)
   calbframe = color_calibration(frame, calib_gain) 
   cv2.imshow("calib image", calbframe)
@@ -60,7 +63,10 @@ while True:
   cv2.setMouseCallback("calib image", picker, calbframe)
   cv2.setMouseCallback("Frame image", picker, frame)
 
+  print("> calib colour",calbframe[pix_y,pix_x])
   key=cv2.waitKey(30) & 0xFF
+  if key == 32: # space for capture image
+    capture_disable = ~capture_disable
   if key == 27: # exit on ESC
     # cleanup the camera and close any open windows
     cap.release()
